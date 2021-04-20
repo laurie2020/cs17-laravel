bcbfbc20c085570f5bfa7dcfd976d060d0bb38e3
Laurie <laurieiula2016@gmail.com>
2021-04-20 09:08:19 +0200

    intro

---
+++ introduction/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get("/", function() {
+    $prenom = "Laurie";
+    $nom = "Iula";
+    return view("index", compact("prenom", "nom"));
+});
+
+
+Route::get("/contact", function() {
+    $tableau = [1, 2, 3];
+    $objet = new stdClass();
+    $objet->nom = "Iula";
+    $objet->prenom = "Laurie";
+    return view("contact", compact("objet"));
+});
+
+Route::get("/{chiffre}", function($chiffre){
+    return "Le chiffre que j'ai ajouté est $chiffre";
+});
