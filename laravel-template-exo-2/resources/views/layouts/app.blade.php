3c30a62d6da21cf63947f42b136d6a22ddc9b4d6
Laurie <laurieiula2016@gmail.com>
2021-04-23 11:20:36 +0200

    template 1 et 2

---
+++ laravel-template-exo-2/resources/views/layouts/app.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Freelancer - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href={{ asset("img/favicon.ico") }} />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href={{ asset("css/app.css") }} rel="stylesheet" />
+    </head>
+    <body id="page-top">
+
+        @yield('content')
+        <!-- Navigation-->
+        
+        <!-- Masthead-->
+        
+        <!-- Portfolio Section-->
+        
+        <!-- About Section-->
+        
+        
+        <!-- Contact Section-->
+        
+        <!-- Footer-->
+        
+        <!-- Copyright Section-->
+        
+        <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
+        
+        <!-- Portfolio Modals-->
+        <!-- Portfolio Modal 1-->
+        
+        <!-- Portfolio Modal 2-->
+        
+        <!-- Portfolio Modal 3-->
+        
+        <!-- Portfolio Modal 4-->
+        
+        <!-- Portfolio Modal 5-->
+        
+        <!-- Portfolio Modal 6-->
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Contact form JS-->
+        <script src={{ asset("mail/jqBootstrapValidation.js") }}></script>
+        <script src={{ asset("mail/contact_me.js") }}></script>
+        <!-- Core theme JS-->
+        <script src={{ asset("js/app.js") }}></script>
+    </body>
+</html>
---
+++ laravel-template-exo-2/resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    @include('navigation')
+    @include('masthead')
+    @include('portofolio')
+    @include('about')
+    @include('contact')
+    @include('footer')
+    @include('copyright')
+    @include('scroll')
+    @include('modals.modal1')
+    @include('modals.modal2')
+    @include('modals.modal3')
+    @include('modals.modal4')
+    @include('modals.modal5')
+@endsection
